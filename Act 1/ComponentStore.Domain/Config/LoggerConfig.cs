32bdd11d4bab2b07e3b7c1f372eba68f62cd8fa3
LuisDS25 <luisfh007@gmail.com>
2026-06-19 14:18:00 -0600

    Config and Exceptions directory created

---
+++ Act 1/ComponentStore.Domain/Config/LoggerConfig.cs
@@ -0,0 +1,13 @@
+using Serilog;
+
+public static class LoggerConfig
+{
+    public static void Configure()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            //.WriteTo.File("logs/app.log", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+    }
+}
