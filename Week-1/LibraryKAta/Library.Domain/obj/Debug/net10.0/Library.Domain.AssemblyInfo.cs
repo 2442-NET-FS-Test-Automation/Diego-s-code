54f42b4f583d78b439f66ec0f3e45ea574d55bec
Jorge Ramirez <jorgermzc16@gmail.com>
2026-06-22 12:37:32 -0600

    Add pricesapi

--- Week-1/LibraryKAta/Library.Domain/obj/Debug/net10.0/Library.Domain.AssemblyInfo.cs
+++ Week-1/LibraryKAta/Library.Domain/obj/Debug/net10.0/Library.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ebac203e5539bd009cfe6f3314a3a79acde32f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cc8f6070d88f470d893f9b7aa24e925f46560bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
